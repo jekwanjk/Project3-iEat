12456ad8f1be55b1085290c5ce68381d9c3c44f4
Amanda Irazabal <amanda.irazabal@gmail.com>
2020-06-30 11:31:16 -0400

    Added router base

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+// import navbar
 import SignUp from "./pages/SignUp.js";
 import Recipes from "./pages/Recipes.js";
 import Shopping from "./pages/Shopping.js";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      <SignUp />
-      {/* <Recipes /> */}
-      <Shopping />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={SignUp} />
+        <Route exact path="/recipes" component={Recipes} />
+        <Route exact path="/shopping" component={Shopping} />
+      </div>
+    </Router>
   );
 }
 export default App;
