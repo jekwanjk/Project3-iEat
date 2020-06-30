f876dad300e3b824d4ac44ea9d3ee49b80b83106
Amanda Irazabal <amanda.irazabal@gmail.com>
2020-06-30 11:17:35 -0400

    Added router to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+// import navbar
 import SignUp from "./pages/SignUp.js";
 import Recipes from "./pages/Recipes.js";
 import Shopping from "./pages/Shopping.js";
+
 function App() {
   return (
-    <div>
-      <SignUp />
-      {/* <Recipes /> */}
-      <Shopping />
-    </div>
+    <Router>
+      <div>
+        {/* ADD NAVBAR */}
+        <Route exact path="/" component={SignUp} />
+        <Route exact path="/recipes" component={Recipes} />
+        <Route exact path="/shopping" component={Shopping} />
+      </div>
+    </Router>
   );
 }
 export default App;
