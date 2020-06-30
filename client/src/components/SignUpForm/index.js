970e4e2c41dd8dc7573559a7d23a39de23badd23
Amanda Irazabal <amanda.irazabal@gmail.com>
2020-06-30 12:06:33 -0400

    Added missing fields in sign-up form

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -6,15 +6,15 @@ function SignUpForm(props) {
   return (
     <form className="signup">
       <div className="form-group">
-        <label htmlFor="userName">User Name:</label>
+        <label htmlFor="name">Full Name:</label>
         <input
-          value={props.userName}
+          value={props.name}
           onChange={props.handleInputChange}
-          name="userName"
+          name="name"
           type="text"
           className="form-control"
-          placeholder="Type in user name"
-          id="userName"
+          placeholder="Type in your name"
+          id="name"
         />
         <label htmlFor="email">Email:</label>
         <input
@@ -36,6 +36,92 @@ function SignUpForm(props) {
           placeholder="Type in your password"
           id="password"
         />
+        <label htmlFor="calories">Caloric Intake</label>
+        <input
+          value={props.calories}
+          onChange={props.handleInputChange}
+          name="calories"
+          type="text"
+          class="form-control"
+          id="calories"
+          placeholder="Caloric Intake (Ex: 2500, 2000, etc...)"
+        />
+
+        <label for="restrict-option">
+          What kind of dietary restrictions do you have?
+        </label>
+
+        <select
+          value={props.dietRestrictions}
+          onChange={props.handleInputChange}
+          class="form-control"
+          id="restrict-option"
+        >
+          <option>Dairy</option>
+          <option>Egg</option>
+          <option>Gluten</option>
+          <option>Grain</option>
+          <option>Peanut</option>
+          <option>Seafood</option>
+          <option>Sesame</option>
+          <option>Shellfish</option>
+          <option>Soy</option>
+          <option>Sulfite</option>
+          <option>Tree Nut</option>
+          <option>Wheat</option>
+        </select>
+
+        <label for="diet-option">
+          What kind of diet are you interested in?
+        </label>
+        <select
+          value={props.dietType}
+          onChange={props.handleInputChange}
+          class="form-control"
+          id="diet-option"
+        >
+          <option>Gluten Free</option>
+          <option>Ketogenic</option>
+          <option>Vegetarian</option>
+          <option>Lacto-Vegetarian</option>
+          <option>Ovo-Vegetarian</option>
+          <option>Vegan</option>
+          <option>Pescatarian</option>
+          <option>Paleo</option>
+          <option>Primal</option>
+          <option>Whole30</option>
+        </select>
+
+        <label for="city-input">City</label>
+        <input
+          value={props.city}
+          onChange={props.handleInputChange}
+          type="text"
+          class="form-control"
+          id="city-input"
+          placeholder="City"
+        />
+
+        <label for="state-input">State</label>
+        <input
+          value={props.state}
+          onChange={props.handleInputChange}
+          type="text"
+          class="form-control"
+          id="state-input"
+          placeholder="State"
+        />
+
+        <label for="zipcode-input">Zip Code</label>
+        <input
+          value={props.zipCode}
+          onChange={props.handleInputChange}
+          type="text"
+          class="form-control"
+          id="zipcode-input"
+          placeholder="Zip Code"
+        />
+
         <button
           type="submit"
           onClick={props.handleFormSubmit}
