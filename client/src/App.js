31c450e81446f49ee67baec4041e28a70b41ad83
Jeremy Kwan <JeremyKwan@Jeremys-MacBook-Pro.local>
2020-07-01 11:36:32 -0400

    updated login name in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import SignUp from "./pages/SignUp.js";
 import Recipes from "./pages/Recipes.js";
 import Shopping from "./pages/Shopping.js";
 import Navbar from "./components/Navbar";
-import LogIn from "./pages/LogIn.js";
+import Login from "./pages/Login.js";
 import LogOut from "./components/LogOut";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Navbar />
         <Route exact path="/" component={SignUp} />
         <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/recipes" component={Recipes} />
         <Route exact path="/shopping" component={Shopping} />
         <Route exact path="/logout" component={LogOut} />
