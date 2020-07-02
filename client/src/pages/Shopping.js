c59d7dfd8eb928b9dfc63e2e8eedbe844d9e3ecb
Jeremy Kwan <JeremyKwan@Jeremys-MacBook-Pro.local>
2020-07-02 10:35:47 -0400

    updated shopping page name appears

--- client/src/pages/Shopping.js
+++ client/src/pages/Shopping.js
@@ -1,75 +1,44 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 import ShoppingList from "../components/ShoppingList";
-import "antd/dist/antd.css";
-import { Checkbox } from "antd";
 
 function Shopping() {
-  // const [user, setUser] = useState({
-  //   name: "Amanda",
-  //   dietRestrictions: "peanuts",
-  //   calories: 2000,
-  //   dietType: "Gluten Free",
-  //   recipes: []
-  // });
-  const [user, setUser] = useState({
-    name: "",
-    dietRestrictions: "",
-    calories: 0,
-    dietType: "",
-    recipes: []
-  });
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     console.log("useEffect");
-    getCurrentUser();
-  }, []);
-
-  useEffect(() => {
-    console.log("useEffect");
-    loadRecipes();
-  }, []);
-
-  const getCurrentUser = () => {
-    API.getUserInfo(user)
+    // getCurrentUser();
+    API.getUserInfo()
       .then((res) => {
         console.log("getCurrentUser - res.data", res.data);
-        setUser({ ...user, ...res.data });
+        setUser({
+          calories: res.data.calories,
+          city: res.data.calories,
+          dietRestrictions: res.data.dietRestrictions,
+          dietType: res.data.dietType,
+          email: res.data.email,
+          name: res.data.name,
+          password: res.data.password,
+          recipes: res.data.recipes,
+          state: res.data.state,
+          zipCode: res.data.zipCode,
+          _id: res.data._id
+        });
+        console.log("setUser - user", user);
       })
       .catch((err) => {
         console.log("getCurrentUser", err);
         window.location.replace("/login");
       });
-  };
-
-  const loadRecipes = () => {
-    API.getRecipes(user)
-      .then((res) => {
-        createRecipies(res.data[0].recipes);
-      })
-      .catch((err) => console.log("loadRecipes ERROR", err));
-  };
-
-  const createRecipies = (recipes) => {
-    setUser({ ...user, recipes: [...recipes] });
-    console.log("createRecipe - user recipes", user.recipes);
-  };
+  }, []);
 
   const handleSubmitBtn = (event) => {
     event.preventDefault();
-    getCurrentUser();
-    loadRecipes();
   };
-
-  function onChange(e) {
-    console.log(`checked = ${e.target.checked}`);
-  }
 
   return (
     <div>
-      <center>
-        <h1 style={{ color: "white" }}>{user.name}'s Shopping List</h1>
-      </center>
+      <h1>{user.name}'s Shopping List</h1>
       <ShoppingList
         handleSubmitBtn={handleSubmitBtn}
         userName={user.userName}
@@ -84,9 +53,7 @@ function Shopping() {
                   </li>
                 ) : (
                   <li className="list-group-item">
-                    <Checkbox onChange={onChange}>
-                      {item} {recipe.qty[index]} {recipe.units[index]}
-                    </Checkbox>
+                    {item} {recipe.qty[index]} {recipe.units[index]}
                   </li>
                 )
               )
