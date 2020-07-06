386003d38d0ffe39f2cffb860d93dd58fad699c2
Jeremy Kwan <JeremyKwan@Jeremys-MacBook-Pro.local>
2020-07-06 13:14:43 -0400

    updated server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const publicPath = path.join(__dirname, "..", "public");
 
 const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 // Express Session
