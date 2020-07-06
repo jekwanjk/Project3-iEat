37e80fcf0da191381eb45068509bcaf031fe0404
Jeremy Kwan <JeremyKwan@Jeremys-MacBook-Pro.local>
2020-07-06 13:21:47 -0400

    updated server

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static("client/build"));
 }
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
